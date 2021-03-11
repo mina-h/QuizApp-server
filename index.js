4b8aacd2efc1a18e029a64c183fa6ed54010d5b1
Mina Hosseini <minaa.hosseinii@gmail.com>
2021-03-11 12:07:39 +0100

    generate random questions for all continents

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require ('express');
-const fakeData = require('./data');
 const cors = require('cors');
 const fetch = require('node-fetch');
 
@@ -8,6 +7,77 @@ const app = express();
 
 app.use(cors());
 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const generateQuestions = (data, num) => {
+  const randomNumber = randomInteger(1, num);
+  const q1 = data[randomNumber];
+  const q2 = data[randomNumber + 1];
+  const q3 = data[randomNumber + 2];
+  const q4 = data[randomNumber + 3];
+  const q5 = data[randomNumber + 4];
+
+  let questions = [
+    {
+      number: 1,
+      question: q1.flag,
+      answer: q1.capital,
+      options: [
+        q1.capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+      ]
+    },
+    {
+      number: 2,
+      question: q2.flag,
+      answer: q2.capital,
+      options: [
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+        q2.capital,
+        data[randomInteger(1,50)].capital,
+      ]
+    },
+    {
+      number: 3,
+      question: q3.flag,
+      answer: q3.capital,
+      options: [
+        q3.capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+      ]
+    },
+    {
+      number: 4,
+      question: q4.flag,
+      answer: q4.capital,
+      options: [
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+        q4.capital,
+      ]
+    },
+    {
+      number: 5,
+      question: q5.flag,
+      answer: q5.capital,
+      options: [
+        data[randomInteger(1,50)].capital,
+        q5.capital,
+        data[randomInteger(1,50)].capital,
+        data[randomInteger(1,50)].capital,
+      ]
+    }
+  ]
+  return questions;
+}
 
 app.get('/api', (req, res) => {
   if (!req.query.continent){
@@ -15,80 +85,22 @@ app.get('/api', (req, res) => {
   } else {
     const fetchFromRestCountry = async (url) => {
       const response = await fetch(url);
-      const data = await response.json();
+      const fetchedData = await response.json();
 
-      function randomInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
       let questions = [];
 
       if (req.query.continent.toLowerCase() === 'asia') {
-        const randomNumber = randomInteger(1, 40);
-        const q1 = data[randomNumber];
-        const q2 = data[randomNumber + 1];
-        const q3 = data[randomNumber + 2];
-        const q4 = data[randomNumber + 3];
-        const q5 = data[randomNumber + 4];
-
-        questions = [
-          {
-            number: 1,
-            question: q1.flag,
-            answer: q1.capital,
-            options: [
-              q1.capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-            ]
-          },
-          {
-            number: 2,
-            question: q2.flag,
-            answer: q2.capital,
-            options: [
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-              q2.capital,
-              data[randomInteger(1,50)].capital,
-            ]
-          },
-          {
-            number: 3,
-            question: q3.flag,
-            answer: q3.capital,
-            options: [
-              q3.capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-            ]
-          },
-          {
-            number: 4,
-            question: q4.flag,
-            answer: q4.capital,
-            options: [
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-              q4.capital,
-            ]
-          },
-          {
-            number: 5,
-            question: q5.flag,
-            answer: q5.capital,
-            options: [
-              data[randomInteger(1,50)].capital,
-              q5.capital,
-              data[randomInteger(1,50)].capital,
-              data[randomInteger(1,50)].capital,
-            ]
-          }
-        ]
+        questions = generateQuestions(fetchedData, 45);
+      } else if (req.query.continent.toLowerCase() === 'africa'){
+        questions = generateQuestions(fetchedData, 55);
+      } else if (req.query.continent.toLowerCase() === 'europe'){
+        questions = generateQuestions(fetchedData, 45);
+      } else if (req.query.continent.toLowerCase() === 'americas'){
+        questions = generateQuestions(fetchedData, 50);
+      } else if (req.query.continent.toLowerCase() === 'oceania'){
+        questions = generateQuestions(fetchedData, 20);
       }
-      console.log(questions);
+      
       res.send(questions);
     
     }
